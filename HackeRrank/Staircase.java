47f5159693f35dc8808b7282fba2cdaa3b864a9e
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2024-03-16 19:22:11 +0530

    Create Staircase.java

---
+++ HackeRrank/Staircase.java
@@ -0,0 +1,13 @@
+class Result {
+    public static void staircase(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            System.out.print(" ");
+        }
+        for(int k = 0; k <= i; k++) {
+            System.out.print("#");
+        }
+        System.out.println();
+    }
+    }
+}
