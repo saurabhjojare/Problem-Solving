4b60fcdde3c9700151f896fa8ab2839904537834
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2024-03-16 19:13:29 +0530

    Create Plus Minus.java

---
+++ HackeRrank/Plus Minus.java
@@ -0,0 +1,24 @@
+ public static void plusMinus(List<Integer> arr) {
+    int total = arr.size();
+    int positiveCount = 0;
+    int negativeCount = 0;
+    int zeroCount = 0;
+    
+    for(int num : arr) {
+        if(num>0) {
+            positiveCount++;
+        } else if(num<0) {
+            negativeCount++;
+        } else {
+            zeroCount++;
+        }
+    }
+    
+    double positiveRatio = (double) positiveCount/total;
+    double negativeRatio = (double) negativeCount/total;
+    double zeroRatio = (double) zeroCount/total;
+    
+    System.out.printf("%.6f\n",positiveRatio);
+    System.out.printf("%.6f\n",negativeRatio);
+    System.out.println(String.format("%.6f", zeroRatio));
+    }
