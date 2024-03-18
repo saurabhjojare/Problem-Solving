a54921fc51469ef6dafb9821e17a9ca9e44583d9
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2024-03-18 12:39:37 +0530

    Update Birthday Cake Candles.java

--- HackeRrank/Birthday Cake Candles.java
+++ HackeRrank/Birthday Cake Candles.java
@@ -1,7 +1,6 @@
 class Result {
     public static int birthdayCakeCandles(List<Integer> candles) {
         int maxHeight = 0;
-        
         for (int candle : candles) {
             if(candle > maxHeight) {
                 maxHeight = candle;
