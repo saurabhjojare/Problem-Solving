b4f7ff2e39f1b1872e6f2aedcb31cd95c79304c1
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2024-03-20 09:39:49 +0530

    Create Time Conversion.java

---
+++ HackeRrank/Time Conversion.java
@@ -0,0 +1,15 @@
+class Result {
+    public static String timeConversion(String s) {
+       DateFormat inputFormat = new SimpleDateFormat("hh:mm:ssa");
+       DateFormat outputFormat = new SimpleDateFormat("HH:mm:ss");
+       
+       try{
+           Date date = inputFormat.parse(s);
+           String convertedTime = outputFormat.format(date);
+           return convertedTime;
+       } catch(ParseException e) {
+           return "Invalid input format";
+       }
+
+    }
+}
